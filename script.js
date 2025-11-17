f9a2ecce86b871cdeb2057b374bda00e0ae06db1
Lokesh <lokin7778@gmail.com>
2025-11-17 14:03:24 -0500

    Wrote the body of the function which contains switch-case statemnets that willrandomly return either rock or paper or scissors and returned it to the function

--- script.js
+++ script.js
@@ -6,12 +6,32 @@
  */
 
 // create a new function named getComputerChoice
+/**
+ * @param - no parameter
+ * @returns - random choice of rock, paper or scissor.
+ */
 
 function getComputerChoice(){
-    let compChoiceNum = Math.round(Math.random()*2);
-    console.log(compChoiceNum);
+    let compChoiceNum = Math.round(Math.random()*2); // generates a random number between 0 and 2 
+    
+    let compChoice;
+
+    // switch case statement to check if the compChoiceNum equals something and then assigning the final computer's choice (compChoice) to hold either rock,paper or scissors.
+    switch(compChoiceNum){
+        case 0:
+            compChoice = "Rock";
+            break;
+
+        case 1:
+            compChoice = "Paper";
+            break;
+        
+        case 2:
+            compChoice = "Scissors";
+            break;
+    }
+
+    return compChoice; // returns the variable compChoice
 }
 
-getComputerChoice();
-getComputerChoice();
-getComputerChoice();
+console.log(getComputerChoice());
