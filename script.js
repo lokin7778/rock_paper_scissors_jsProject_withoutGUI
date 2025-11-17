ab23c1e88ff64ca36ac33d281202e41979923b99
Lokesh <lokin7778@gmail.com>
2025-11-17 13:56:54 -0500

    Declared a function getComputerChoice which uses the Math.random method to generate random numbers. here i have used Math.round(Math.random()*2)) as this returns natural numbers from 0 to 2, so we have 3 options (rock, paper, scissor)

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 /**
- * SMALL DESCRIPTION OF THE PROGRAM - This program is a small game of rock, paper scissors     which is played on the console by the user and which does not include and GUI as of now. 
+ * SMALL DESCRIPTION OF THE PROGRAM - This program is a small game of rock, paper scissors which is played on the console by the user and which does not include and GUI as of now. 
  * 
  * @author - Lokesh Narasimhan
  * @version - 11/17/2025
  */
 
+// create a new function named getComputerChoice
+
+function getComputerChoice(){
+    let compChoiceNum = Math.round(Math.random()*2);
+    console.log(compChoiceNum);
+}
+
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
