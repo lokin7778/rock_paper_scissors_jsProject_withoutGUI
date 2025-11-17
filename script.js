9c939fcd89dbab6270fa94148854f57425969fd5
Lokesh <lokin7778@gmail.com>
2025-11-17 13:48:28 -0500

    Added the small description of the program

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * SMALL DESCRIPTION OF THE PROGRAM - This program is a small game of rock, paper scissors     which is played on the console by the user and which does not include and GUI as of now. 
+ * 
+ * @author - Lokesh Narasimhan
+ * @version - 11/17/2025
+ */
+
